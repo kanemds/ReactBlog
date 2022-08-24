41864e72701c5e13a79061e973e305fb3527ce7b
Kanem <kanemdeng@email.com>
2022-08-24 10:52:22 -0700

    created custom error message

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,17 +6,28 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
 
   useEffect(() => {
     setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
+      fetch('http://localhost:8000/blogss')
         .then(res => {
+          //res.ok is a property
+          if (!res.ok) {
+            //custom error message
+            throw Error('Data not found or Connection problem')
+          }
           return res.json()
         })
         .then((data) => {
           setBlogs(data)
           setIsPending(false)
+          setError(null)
+        })
+        .catch((err) => {
+          setIsPending(false)
+          setError(err.message)
         })
     }, 2000)
   }, [])
@@ -25,6 +36,7 @@ const Home = () => {
     <div className='home'>
       {/* the inital blogs is null, it's error due to the mapping on BlogList
        by doing blogs &&, it first make sure blogs exist then run the right */}
+      {error && <Typography variant="h5" >{error} </Typography>}
       {isPending && <Typography variant="h5" >Loading...</Typography>}
 
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}

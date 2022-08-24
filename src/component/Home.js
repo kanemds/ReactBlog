cab7f55e092a3e6e9f7c9db33122148e8b0ebb4b
Kanem <kanemdeng@email.com>
2022-08-24 10:00:24 -0700

    made setTimeout for 2s testing the loading... screen

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import BlogList from './BlogList'
+import { Typography } from '@mui/material'
 
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null)
-
+  const [isPending, setIsPending] = useState(true)
 
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then((data) => {
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json()
+        })
+        .then((data) => {
+          setBlogs(data)
+          setIsPending(false)
+        })
+    }, 2000)
   }, [])
 
   return (
     <div className='home'>
       {/* the inital blogs is null, it's error due to the mapping on BlogList
        by doing blogs &&, it first make sure blogs exist then run the right */}
+      {isPending && <Typography variant="h5" >Loading...</Typography>}
+
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
 
 

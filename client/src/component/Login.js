e01e7cc477a81f3e76b7ba117c28e036501fc4e9
Kanem <kanemdeng@email.com>
2022-08-29 12:43:46 -0700

    update

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Paper, Typography, FormControl, TextField, Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import AuthContext from '../context/ AuthProvider'
-import { response } from 'express'
+
 
 const Login = () => {
 
@@ -20,9 +20,15 @@ const Login = () => {
     setShow(false)
   }, [user, pwd])
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
+
+    if (!user || !pwd) {
+      setShow(true)
+      setErrMsg('User and Email are required')
+    }
+
     try {
-      fetch('http://localhost:3001/users', {
+      const loginUser = await fetch('http://localhost:3001/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         withCredentials: true,
@@ -31,18 +37,18 @@ const Login = () => {
           password: pwd
         })
       })
-        .then(response => response.json())
-      console.log(response)
-        .then(() => {
-          console.log('Success')
-        })
-
+      if (loginUser.status < 400) {
+        const content = await loginUser.json()
+        // console.log(loginUser)
+        console.log(content)
+        setShow(false)
+      }
     } catch (error) {
       if (!error?.response) {
         setErrMsg('No Server Response')
         setShow(true)
       } else if (error.response?.status === 400) {
-        setErrMsg('Missing User name or Password')
+        setErrMsg('User and Email are required')
         setShow(true)
       } else if (error.response?.status === 401) {
         setErrMsg('Unauthorized')
@@ -51,21 +57,22 @@ const Login = () => {
         setShow(true)
       }
     }
-
   }
+
+
 
   return (
     <>
       <Paper sx={{ p: 3 }}>
-        {show ? "" :
-          <Typography> {errMsg}</Typography>
+        {!show ? "" :
+          <Typography sx={{ backgroundColor: 'pink', p: 1 }}> {errMsg}</Typography>
         }
         <Typography variant="h4">Login</Typography>
         <FormControl fullWidth>
           <TextField sx={{ m: 1 }} type="text" autoComplete='off' id="standard-basic" label="User Name" variant="outlined"
             onChange={e => setUser(e.target.value)} value={user} required
           />
-          <TextField sx={{ m: 1 }} id="standard-basic" label="Password" variant="outlined"
+          <TextField sx={{ m: 1 }} id="standard-basic" label="Password" type="password" variant="outlined"
             onChange={e => setPwd(e.target.value)} value={pwd} required />
         </FormControl>
         <Button onClick={() => navigate('/')}>Cancel</Button>
